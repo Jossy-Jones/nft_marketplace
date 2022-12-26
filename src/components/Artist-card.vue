9e67272db5a8f6e1995e3e8a1d4a9de807d8ed18
Jossy-Jones <jossyjones187@gmail.com>
2022-12-26 15:42:39 -0500

    Initialization...

---
+++ src/components/Artist-card.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "ArtistCard",
+    props: {
+        "artistData": {
+            type: Object,
+            required: true,
+        },
+        "ranking":{
+            type: Boolean
+        }
+    },
+    data(){
+        return{
+            rank: parseInt(this.artistData.id),
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="relative flex items-center lg:grid gap-4 px-6 py-4 lg:py-6 rounded-lg lg:text-center bg-backgroundSecond">
+        <span v-if="ranking" class="absolute top-2 left-4 flex items-center justify-center w-7 h-7 bg-background text-labelText rounded-full">{{rank}}</span>
+        <div class="shrink-0 inline-block lg:mx-auto w-16 h-16 lg:w-32 lg:h-32 rounded-full">
+            <img :src="'/img/artists/'+artistData.image" />
+        </div>
+        <div class="grid gap-1.5">
+            <h3 class="text-textColor text-2xl font-bold">{{artistData.name}}</h3>
+            <p class="inline-flex lg:inline-block gap-4 text-lg text-labelText whitespace-nowrap">Total Sales: <span class="text-textColor font-bold">{{artistData.sales}} ETH</span></p>
+        </div>
+    </div>
+  
+</template>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#A259FF',
+        background: '#2B2B2B',
+        backgroundSecond: '#3B3B3B',
+        labelText: '#858584',
+        textColor: '#FFFFFF',
+      }
+    },
+  },
+  plugins: [],
+}
